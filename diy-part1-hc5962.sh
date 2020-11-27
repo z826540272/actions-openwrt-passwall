c151f605795c836e420bda21db623abb80d5d751
ChenChen <cc2286927@sina.cn>
2020-11-27 18:29:23 +0800

    Create diy-part1-hc5962.sh

---
+++ diy-part1-hc5962.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+sed -i 's/192.168.1.1/4.1.1.1/g' package/base-files/files/bin/config_generate
+# mkdir package/base-files/files/config
+# echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+rm -rf package/lean/smartdns
+# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
