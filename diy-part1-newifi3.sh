bd038e60789dd3a56481e9547c510b8ccea9cc71
z826540272 <56025847+z826540272@users.noreply.github.com>
2021-04-21 11:25:44 +0000

    add for bypass

--- diy-part1-newifi3.sh
+++ diy-part1-newifi3.sh
@@ -8,7 +8,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.10/g' package/base-files/files/bin/config_generate
 # mkdir package/base-files/files/config
-# echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 # rm -rf package/lean/smartdns
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 # git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
@@ -17,3 +16,6 @@ sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
+git clone https://github.com/garypang13/smartdns-le package/smartdns-le
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
