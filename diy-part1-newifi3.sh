7d50fd52168f64a45d6e0964096c1c884f5edf2d
z826540272 <56025847+z826540272@users.noreply.github.com>
2021-04-21 11:15:16 +0000

    add luci-app-bypass

--- diy-part1-newifi3.sh
+++ diy-part1-newifi3.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+sed -i 's/192.168.1.1/10.0.0.10/g' package/base-files/files/bin/config_generate
+# mkdir package/base-files/files/config
+# echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+# rm -rf package/lean/smartdns
+# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+
+git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
