26aca9d91adc32eb63a0d44870d69cba8d7191f1
z826540272 <56025847+z826540272@users.noreply.github.com>
2021-09-01 19:55:12 +0800

    Update diy-part1-lean.sh

--- diy-part1-lean.sh
+++ diy-part1-lean.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+git config --global user.email "826540272@qq.com"
+git config --global user.name "826540272"
+git revert -n 634663fe5663c40893ce6527e48af170c80d5626
+git revert -n a74c473f42394c02695f909f1cbf9283b10dfc5d
+
 sed -i 's/192.168.1.1/10.0.0.10/g' package/base-files/files/bin/config_generate
 # DNS Cache FixUP
 echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600\nauth-ttl=3600" >> package/network/services/dnsmasq/files/dnsmasq.conf
@@ -20,6 +20,3 @@ echo -e "#max-ttl=600\nneg-ttl=600\nmin-cache-ttl=3600\nauth-ttl=3600" >> packag
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
-#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-#git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
-#git clone https://github.com/destan19/OpenAppFilter.git package/lean/OpenAppFilter
