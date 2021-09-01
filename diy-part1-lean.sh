b1e0a86ac5b03506c6ab6d75d44ad4a0d8c1509a
z826540272 <56025847+z826540272@users.noreply.github.com>
2021-09-01 23:34:19 +0800

    Update diy-part1-lean.sh

--- diy-part1-lean.sh
+++ diy-part1-lean.sh
@@ -4,10 +4,6 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-git config --global user.email "826540272@qq.com"
-git config --global user.name "826540272"
-git revert -n 634663fe5663c40893ce6527e48af170c80d5626
-git revert -n a74c473f42394c02695f909f1cbf9283b10dfc5d
 
 sed -i 's/192.168.1.1/10.0.0.10/g' package/base-files/files/bin/config_generate
 # DNS Cache FixUP
